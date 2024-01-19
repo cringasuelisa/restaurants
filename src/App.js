d1bcec767762e165b209436b75bd9053203a3836
cringasuelisa <cringasuelisa@gmail.com>
2024-01-19 17:38:19 +0200

    n avem harta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Ritem from './components/Ritem';
 import './App.css';
 
+
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/restaurants')
+      .then((response) => response.json())
+      .then((data) => setRestaurants(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='containerApp'>
+    <div className='coloanaRestaurante'>
+      <h1>Lista Restaurante</h1>
+      {restaurants.map((restaurant) => (
+        <Ritem item={restaurant}/>
+      ))}
     </div>
+    <div className='harta'>
+        <div>
+   
+        </div>
+    </div>
+  </div>
+   
   );
 }
 
